8c82810199bcecf9592e3148ee414026381d03d4
Lina-Sc <125125270+Lina-Sc@users.noreply.github.com>
2023-02-24 10:21:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,14 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    if (a < b) {
+        swap(a, b);
+    }
+
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+
+    return a;
 }
